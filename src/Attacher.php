066b7e16734eff4db85f3b3d0d72d44ac1e9559c
Joseph Schilz <joseph@schilz.org>
2018-02-26 21:22:54 -0800

    Return headers.

--- src/Attacher.php
+++ src/Attacher.php
@@ -42,21 +42,36 @@ class Attacher
         return $result;
     }
 
-    protected function post($location, $body, $headers = [])
+    protected function post($location, $body, $requestHeaders = [], &$responseHeaders = [])
     {
         $options = [
             CURLOPT_URL => rtrim($this->apibase, '/') . '/' . $location,
             CURLOPT_POST => true,
             CURLOPT_POSTFIELDS => $body,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTPHEADER => $requestHeaders,
+            CURLOPT_HEADERFUNCTION => function($curl, $header) use (&$responseHeaders)
+            {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) // ignore invalid headers
+                    return $len;
+
+                $name = strtolower(trim($header[0]));
+                if (!array_key_exists($name, $responseHeaders))
+                    $responseHeaders[$name] = [trim($header[1])];
+                else
+                    $responseHeaders[$name][] = trim($header[1]);
+
+                return $len;
+            },
         ];
 
         $ch = curl_init();
         curl_setopt_array($ch, ($this->defaultOptions + $options));
 
         $result = curl_exec($ch);
-        print_r($result);
         $result = json_decode($result, true);
 
         curl_close($ch);
@@ -94,17 +109,17 @@ class Attacher
     {
         $location = 'datatel_supplementalinformationsubmissions';
         $data = [
-            "datatel_supplementalinformationitemid@odata.bind" => "/datatel_supplementalinformationitems($informationItemId)",
-            "datatel_prospectid@odata.bind" => "/contacts($contactId)",
+            "datatel_suppinfoitem_suppinfosubmission@odata.bind" => "/datatel_supplementalinformationitems($informationItemId)",
+            "datatel_contact_datatel_supplementalinformationsu@odata.bind" => "/contacts($contactId)",
             "datatel_name" => "Waitlist Confirmation Form",
             "datatel_submissionstatus" => 1,
             "uwit_processingstatus" => 1,
-            "datatel_submissiondate" => (new \DateTime('now'))->format(DATE_ISO8601),
+            //"datatel_submissiondate" => (new \DateTime('now'))->format(DATE_ISO8601),
+            "datatel_submissiondate" => '2018-02-23T07:32:05.4272Z',
         ];
-
         $body = json_encode($data, JSON_UNESCAPED_SLASHES);
 
-        $headers = [
+        $requestHeaders = [
             'Content-Type: application/json; charset=7tf-8',
             'OData-MaxVersion: 4.0',
             'OData-Version: 4.0',
@@ -112,20 +127,25 @@ class Attacher
             'Prefer: odata.include-annotations="*"',
         ];
 
-        print_r($this->post($location, $body, $headers));
+        $responseHeaders = [];
+        $this->post($location, $body, $requestHeaders, $responseHeaders);
+
+        $locationHeader = $responseHeaders['location'][0];
+        $supplementalInformationSubmissionId = substr($locationHeader, strpos($locationHeader, '('));
+        $supplementalInformationSubmissionId = trim($supplementalInformationSubmissionId, '()');
+
+        return $supplementalInformationSubmissionId;
     }
 
     public function attach($syskey, $informationItemName, $documentname, $documentcontent)
     {
         $contactId = $this->getContactId($syskey);
 
-        echo $contactId;
-        die();
         $informationItemId = $this->getSupplementalInformationItemId($informationItemName);
+        $supplementalInformationId = $this->createSupplementalInformationSubmission($contactId, $informationItemId);
 
-        $this->createSupplementalInformationSubmission($contactId, $informationItemId);
+        print($supplementalInformationId);
 
-        echo $informationItemId;
 
         return true;
 
