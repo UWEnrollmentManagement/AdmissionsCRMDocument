4a3edfc2f9c9aff62d6e3a55225059f1711d43c1
Joseph Schilz <joseph@schilz.org>
2018-03-13 17:24:13 -0700

    Documentation.

--- src/Attacher.php
+++ src/Attacher.php
@@ -11,25 +11,62 @@ class Attacher
     /** @var Curler $curler */
     protected $curler = null;
 
+    /** @var string $error */
+    protected $error = '';
+
+    /**
+     * Attacher constructor.
+     *
+     * @param Curler $curler
+     */
     public function __construct(Curler $curler)
     {
         $this->curler = $curler;
     }
 
+    /**
+     * Get an error message from the attacher. Clears the error message.
+     *
+     * If the attaching process fails, then there may be an error available to get.
+     *
+     * @return string
+     */
+    public function getError()
+    {
+        $error = $this->error;
+        $this->error = '';
 
-    protected function getContactId($syskey)
+        return $error;
+    }
+
+    /**
+     * Retrieves the primary key of a contact in the CRM, given their system key.
+     *
+     * @param string $systemKey The contact's system key.
+     * @return string|false     String on success, false on failure.
+     */
+    protected function getContactId($systemKey)
     {
         $location = 'contacts';
         $query = [
             '$select' => 'contactid',
-            '$filter' => "uwit_sdb_system_key eq '$syskey'",
+            '$filter' => "uwit_sdb_system_key eq '$systemKey'",
         ];
 
-        $result = $this->curler->get($location, $query);
+        $result = json_decode($this->curler->get($location, $query), true);
 
-        return $result['value'][0]['contactid'];
+        if (array_key_exists('value', $result) && array_key_exists(0, $result['value'])) {
+            return $result['value'][0]['contactid'];
+        } else {
+            return false;
+        }
     }
 
+    /**
+     * Get the CRM primary key of a supplemental information item, by the item's name.
+     * @param string $informationItemName The name of the supplemental information item.
+     * @return string|false               String on success, false on failure.
+     */
     protected function getSupplementalInformationItemId($informationItemName)
     {
         $location = 'datatel_supplementalinformationitems';
@@ -38,10 +75,46 @@ class Attacher
             '$filter' => "datatel_name eq '$informationItemName'",
         ];
 
-        $result = $this->curler->get($location, $query);
-        return $result['value'][0]['datatel_supplementalinformationitemid'];
+        $result = json_decode($this->curler->get($location, $query), true);
+        if (array_key_exists('value', $result) && array_key_exists(0, $result['value'])) {
+            return $result['value'][0]['datatel_supplementalinformationitemid'];
+        } else {
+            return false;
+        }
     }
 
+    /**
+     * Helper function to retrieve the id of a created entity from the response headers of the POST request
+     * used to create that entity.
+     *
+     * When you create a new entity using POST, the response includes a location header that directs the caller
+     * to the address of the newly created entity. The ID of that entity is inside of parentheses.
+     *
+     * @param array $responseHeaders The response headers received when creating the entity.
+     * @return string|false          The entity's ID on success, false on failure.
+     */
+    protected function getCreatedEntityId($responseHeaders)
+    {
+        if (array_key_exists('location', $responseHeaders)) {
+            $locationHeader = $responseHeaders['location'][0];
+            $entityId = substr($locationHeader, strpos($locationHeader, '('));
+            $entityId = trim($entityId, '()');
+            $entityId = (int)$entityId, '()';
+
+            return $entityId;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Create a supplemental information submission for a contact.
+     *
+     * @param string $contactId           The CRM primary key of the contact to record the submission for.
+     * @param string $informationItemId   The id obtained from getSupplementalInformationItemId()
+     * @param string $informationItemName A name for the information item.
+     * @return string|false
+     */
     protected function createSupplementalInformationSubmission($contactId, $informationItemId, $informationItemName)
     {
         $location = 'datatel_supplementalinformationsubmissions';
@@ -65,15 +138,25 @@ class Attacher
         ];
 
         $responseHeaders = [];
-        $this->curler->post($location, $body, $requestHeaders, $responseHeaders);
+        $responseCode = -1;
+        $this->curler->post($location, [], $body, $requestHeaders, [], $responseHeaders, $responseCode);
 
-        $locationHeader = $responseHeaders['location'][0];
-        $supplementalInformationSubmissionId = substr($locationHeader, strpos($locationHeader, '('));
-        $supplementalInformationSubmissionId = trim($supplementalInformationSubmissionId, '()');
-
-        return $supplementalInformationSubmissionId;
+        if ($responseCode >= 200 && $responseCode < 300) {
+            return $this->getCreatedEntityId($responseHeaders);
+        } else {
+            return false;
+        }
     }
 
+    /**
+     * Attach a document body to a supplemental information submission item.
+     *
+     * @param string $documentBody                        The document of the body to attach, base64 encoded.
+     * @param string $documentName                        The file name of the file to attach.
+     * @param string $documentMimeType                    The document mime type of the file.
+     * @param string $supplementalInformationSubmissionId The Id obtained from createSupplementalInformationSubmission.
+     * @return string|false                               The Id of the annotation crated, or false on failure.
+     */
     protected function attachAnnotation($documentBody, $documentName, $documentMimeType, $supplementalInformationSubmissionId)
     {
         $location = 'annotations';
@@ -94,25 +177,52 @@ class Attacher
         ];
 
         $responseHeaders = [];
-        $response = $this->curler->post($location, $body, $requestHeaders, $responseHeaders);
+        $responseCode = -1;
+        $this->curler->post($location, [], $body, $requestHeaders, [], $responseHeaders, $responseCode);
 
-        print_r($response);
-        print_r($responseHeaders);
-
-        return $response;
-
+        if ($responseCode >= 200 && $responseCode < 300) {
+            return $this->getCreatedEntityId($responseHeaders);
+        } else {
+            return false;
+        }
     }
 
-    public function attach($syskey, $informationItemName, $documentBody, $documentName, $documentMimeType)
+    /**
+     * Attach a document to a student's CRM record.
+     *
+     * @param string $systemKey           The SDB system key of the student to attach this record to.
+     * @param string $informationItemName Must correspond to an existing supplemental information item name in the CRM.
+     * @param string $documentBody        The body the document to attach, encoded in base64.
+     * @param string $documentName        The file name of the document to attach.
+     * @param string $documentMimeType    The mime type of the document to attach.
+     * @return string|false               The Id of the created annotation on success, or false on failure.
+     */
+    public function attach($systemKey, $informationItemName, $documentBody, $documentName, $documentMimeType)
     {
-        $contactId = $this->getContactId($syskey);
+        $contactId = $this->getContactId($systemKey);
+        if ($contactId === false) {
+            $this->error = 'Failed to find contact for given system key.';
+            return false;
+        }
 
         $informationItemId = $this->getSupplementalInformationItemId($informationItemName);
+        if ($informationItemId === false) {
+            $this->error = "Failed to find supplement information item with given name: '$informationItemName'.";
+            return false;
+        }
+
         $supplementalInformationSubmissionId = $this->createSupplementalInformationSubmission($contactId, $informationItemId, $informationItemName);
+        if ($supplementalInformationSubmissionId === false) {
+            $this->error = 'Failed to create supplemental information submission.';
+            return false;
+        }
 
-        $this->attachAnnotation($documentBody, $documentName, $documentMimeType, $supplementalInformationSubmissionId);
+        $annotationId = $this->attachAnnotation($documentBody, $documentName, $documentMimeType, $supplementalInformationSubmissionId);
+        if ($annotationId === false) {
+            $this->error = 'Failed to create annotation.';
+            return false;
+        }
 
-        return true;
-
+        return $annotationId;
     }
 }
